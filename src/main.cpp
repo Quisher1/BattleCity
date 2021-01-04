856855ba33fffa329b36ecd545f5715a28fd86db
Quisher1 <hjrabk@mail.ru>
2021-01-04 23:30:00 +0500

    Opengl #1

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,46 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(void)
-{
-	GLFWwindow* window;
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+
+void glfwWindowSizeCallback(GLFWwindow* window, int width, int height) {
+	g_windowSizeX = width;
+	g_windowSizeY = height;
+	glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+}
+
+int main(void){
 
 	/* Initialize the library */
-	if (!glfwInit())
+	if (!glfwInit()) {
+		std::cout << "glfwInit failed!" << std::endl;
 		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
+	GLFWwindow* window = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "BattleCity", nullptr, nullptr);
+	if (!window){
+		std::cout << "glfwCreateWindow failed" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
+	glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+	//glfwSetKeyCallback(window, glfwKeyCallback);
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
@@ -27,13 +51,14 @@ int main(void)
 		return -1;
 	}
 
-	std::cout << "OpenGL: " << GLVersion.major << "." << GLVersion.minor << std::endl;
+	std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "Opengl version: " << glGetString(GL_VERSION) << std::endl;
+	
 
 	glClearColor(1, 0, 0, 1);
 
 	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
+	while (!glfwWindowShouldClose(window)){
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 		
